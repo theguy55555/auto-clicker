ec3f88d630372ce170994b899bae7dc8670ab76d
tesilio <tesilio@urbanbase.com>
2020-05-21 17:41:18 +0900

    단축키를 변경하고 옵션을 추가한다

--- js/content.js
+++ js/content.js
@@ -1,1 +1,12 @@
-const autoClicker = new AutoClicker();
+new AutoClicker();
+
+
+
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     console.log(sender.tab ?
+//       "from a content script:" + sender.tab.url :
+//       "from the extension");
+//     if (request.greeting === "hello")
+//       sendResponse({farewell: "goodbye"});
+//   });
